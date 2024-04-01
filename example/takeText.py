bd80ad96fb23d7fb4568f08648395a9fd69f70ad
rollurioari <rollurioari@gmail.com>
2024-04-01 13:43:34 +0500

    takeText

--- example/takeText.py
+++ example/takeText.py
@@ -13,17 +13,19 @@ headers = {
     'Accept': stAccept,
     'User-Agent': stUserAgent
 }
-link = 'https://genius.com/Beyonce-and-dolly-parton-tyrant-lyrics'
-req = requests.get(link,headers)
+link = 'https://genius.com/Instasamka-fact-lyrics'
+req = requests.get(link,headers) or print(f'некорректная ссылка {link}')
 
 src = req.text
 
 soup = BeautifulSoup(src,'lxml')
 title = soup.title.string
 print(f'Название: {title}')
-takeAllText = soup.find('div',class_='Lyrics__Container-sc-1ynbvzw-1 kUgSbL').get_text(strip=True)
-#reText = re.sub(r'\<.{1,69}\>','',str(tekeAllText))
-print(takeAllText)
-#textFile = open('textFile.txt','w+')
-#textFile.write(takeAllText)
-#textFile.close()
+takeAllText = soup.find('div',class_='Lyrics__Container-sc-1ynbvzw-1 kUgSbL').get_text(separator=' ')
+reText = re.sub(r'\[.{1,30}\]','',str(takeAllText))
+print(f'Текст: {reText}')
+
+
+textFile = open('textFile.txt','w+')
+textFile.write(f'Название: {title}\nТекст: {reText}')
+textFile.close()
